0e55aefcdc5eee29b8c10167561ae2bcc985fea6
farellrayner <farellrayner@gmail.com>
2022-09-20 09:02:50 +0700

    Initial commit

---
+++ app/src/main/java/com/farell/gdroom_d_10560/packageroom/Constant.kt
@@ -0,0 +1,9 @@
+package com.farell.gdroom_d_10560.packageroom
+
+class Constant {
+    companion object{
+        const val TYPE_READ = 0
+        const val TYPE_CREATE = 1
+        const val TYPE_UPDATE = 2
+    }
+}
---
+++ app/src/main/java/com/farell/gdroom_d_10560/packageroom/Note.kt
@@ -0,0 +1,12 @@
+package com.farell.gdroom_d_10560.packageroom
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+    )
